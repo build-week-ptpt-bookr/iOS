b9c2bb7784f072ce24b962881f81aa2926f3be9d
John Kouris <jdkouris@gmail.com>
2019-08-27 19:32:59 -0600

    Added updateViews() method on the BookCollectionViewCell to update the image and label

--- Bookr/Bookr/Views/BookCollectionViewCell.swift
+++ Bookr/Bookr/Views/BookCollectionViewCell.swift
@@ -13,4 +13,17 @@ class BookCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var bookImageView: UIImageView!
     @IBOutlet weak var bookTitleLabel: UILabel!
     
+    var book: Book? {
+        didSet {
+            updateViews()
+        }
+    }
+    
+    func updateViews() {
+        guard let book = book else { return }
+        
+        bookImageView.image = UIImage(named: book.coverUrl)
+        bookTitleLabel.text = book.title
+    }
+    
 }
